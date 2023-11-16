f4ee79ff6dcc71c7342170310803e5b6bf2cfa42
yigitkayavt <yigitkaya@vt.edu>
2023-11-15 23:55:56 -0500

    final

--- hw3_function.cpp
+++ hw3_function.cpp
@@ -17,13 +17,28 @@ int expo(const int m, const unsigned int n) {
 	funcCallCounter++;  // Do not tounch this line of code.  
 						//This is a global variable helping to count the number of times 
 						//that this function is called
-	
-	
-	
+	//If n = 0 then the answer is automatically 1
+	if (n == 0) {
+		return 1;
+	}
+	//If m = 0, the answer is 0 
+	if (m == 0) {
+		return 0;
+	}
+	//if n is even then do the following
+	else if (n % 2 == 0) {
+		int even = expo(m, n / 2);//using recursive to multiply
+		return even * even;
+	}
+	//if n is odd then do the following
+	else {
+		int odd = expo(m, (n - 1) / 2); //using recursive to multiply
+		return m * odd * odd;
+	}
 
+	
 	//Add your code..............
-
-	return 0;
+	
 }
 
 std::string reverse(const std::string s, const unsigned int size) {
@@ -31,54 +46,118 @@ std::string reverse(const std::string s, const unsigned int size) {
 						//This is a global variable helping to count the number of times 
 						//that this function is called
 
-	
-
 	//Add your code..............
-
-	return("place holder");
-
+	//if the given string exist then reverse it 
+	if (size > 0) {
+		return s.substr(size - 1, 1) + reverse(s.substr(0, size - 1), size - 1);
+	}
+	//if no string exist then return a "" empty character
+	else {
+		return "";
+	}
 }
-
 int minArray(const int anArray[], const  int first, const  int last) {
 	funcCallCounter++;  // Do not tounch this line of code.  
 						//This is a global variable helping to count the number of times 
 						//that this function is called
-
-	//Add your code..............
-
-	return 0;
+	int minValue;
+	//if there is only one item in the array then return the value of the first value
+	if (first == last) {
+		return anArray[first];
+	}
+	//else return the minimum value 
+	else if (first < last) {
+		minValue = anArray[first];
+		int nextValue = minArray(anArray, first + 1, last);
+		if (minValue > nextValue) {
+			minValue = nextValue;
+		}
+	}
+	else {
+		return -1;
+	}
+	return minValue;
 }
+	
 
 int binaryCount(const int anArray[], const  int first, const int last, const int target)
 {
 	funcCallCounter++;  // Do not tounch this line of code.  
-						//This is a global variable helping to count the number of times 
-						//that this function is called
+	//This is a global variable helping to count the number of times 
+	//that this function is called
 
-
+//Add your code..............
+	int count = 0;
+	int mid;
 	
-	//Add your code..............
-
-	return 0;
+	if (first <= last) {
+		//get the mid value of the array
+		mid = first + (last - first) / 2;
+		//if the target is in mid then increase count
+		if (target == anArray[mid]) {
+			count++;
+			//check both sides to make sure there are no duplicates,
+			//and if there are duplicates increase count
+			count += binaryCount(anArray, mid + 1, last, target);
+			count += binaryCount(anArray, first, mid - 1, target);
+		}
+		//if the target is bigger than mid value, increase mid by 1
+		else if (target > anArray[mid]) {
+			count+= binaryCount(anArray,mid + 1,last,target);
+		}
+		//if the target is smaller than mid value, decrease mid by 1
+		else {
+			count += binaryCount(anArray, first, mid - 1, target);
+		}
+	}
+	return count;
 }  
 
 
 int binarySuccessor(const int anArray[], const  int first, const int last, const int target)
 {
 	funcCallCounter++;  // Do not tounch this line of code.  
-						//This is a global variable helping to count the number of times 
-						//that this function is called
+	//This is a global variable helping to count the number of times 
+	//that this function is called
+	
+//Add your code..............
 
-	//Add your code..............
+	int leftSide;
+	if(first <= last) {
+		//get the mid value of the array
+		int mid = first + (last - first) / 2;
+		//if the target is equal to the mid value, then return mid
+		//value since they are equal then it needs to be the smallest value integer
+		if (target == anArray[mid]) {
+			return mid; 
+		}
+		//if target is bigger than the mid value then, increase mid by 1
+		//return the smallest value
+		else if (target > anArray[mid]) {
+			return binarySuccessor(anArray, mid + 1, last, target); 
+		}
+		else {
+			//if target is smaller than the mid value then, decrease the mid value by 1
+			//if there is nothing bigger than target, return the mid value, if there is then 
+			//return the smallest value. 
+			leftSide = binarySuccessor(anArray, first, mid - 1, target);
+			if (leftSide == -1) {
+				return mid;
+			}
+			else {
+				return leftSide;
+			}
 
-	return 0;
+		}
+	}
+	return -1;
 }  
 
 
 
 
 
-void powerSet(std::vector<std::string>& pSet,  const std::string bSet) {
+void powerSet(std::vector<std::string>& pSet,  const std::string bSet, std::string start = "", int i = 0) {
 
 	funcCallCounter++;  // Do not tounch this line of code.  
 							//This is a global variable helping to count the number of times 
@@ -86,8 +165,8 @@ void powerSet(std::vector<std::string>& pSet,  const std::string bSet) {
 
 	//Add your code..............
 	
+
 	
-	return;
 }
 
 
--- hw3_test.cpp
+++ hw3_test.cpp
@@ -1,8 +1,9 @@
 ////////////////////////////////////////////////////////
-// ECE 2574, Homework 3
+// ECE 3514, Project 3, Yigit Kaya
 // File name:   hw3_test.cpp
-// Description: STARTER CODE
-// Date:        2/2019  
+// Description: This file is for testing the functions that 
+//				got implemented in the hw3_function.cpp file
+// Date:        11/15/2023  
 //
 #define CATCH_CONFIG_MAIN			// this line tells Catch to provide a main() function
 #define CATCH_CONFIG_COLOUR_NONE	// this line avoids problems due to color-coding the output
@@ -18,6 +19,33 @@ extern int funcCallCounter; // Do not tounch this line of code.
 
 TEST_CASE("Test Binary Count", "[binaryCount]") {
 
+	funcCallCounter = 0;
+
+	int myArrayWithDuplication[10] = { -1,3,5,5,8 };
+	unsigned int count = binaryCount(myArrayWithDuplication, 0, 4, -5);
+	CHECK(count == 0);
+	REQUIRE(funcCallCounter > 1);
+
+	funcCallCounter = 0;
+	count = binaryCount(myArrayWithDuplication, 0, 4, 5);
+	CHECK(count == 2);
+	REQUIRE(funcCallCounter > 1);
+
+	funcCallCounter = 0;
+	count = binaryCount(myArrayWithDuplication, 0, 4, 3);
+	CHECK(count == 1);
+	REQUIRE(funcCallCounter > 1);
+
+	funcCallCounter = 0;
+	count = binaryCount(myArrayWithDuplication, 0, 4, 9);
+	CHECK(count == 0);
+	REQUIRE(funcCallCounter > 1);
+
+	funcCallCounter = 0;
+	count = binaryCount(myArrayWithDuplication, 0, 4, -2);
+	CHECK(count == 0);
+	REQUIRE(funcCallCounter > 1);
+
 
 	
 
@@ -25,26 +53,128 @@ TEST_CASE("Test Binary Count", "[binaryCount]") {
 
 TEST_CASE("Test Binary Successor", "[binarySuccessor]") {
 
+	funcCallCounter = 0;
 
+	int myArrayWithNoDuplicate[10] = { -5, -3, -1, 0, 1, 5, 9 };
+	int successor = binarySuccessor(myArrayWithNoDuplicate, 0, 8, 4);
+	CHECK(successor != -1);
+	CHECK(successor < 10);
+	if ((successor < 10) && (successor > -1)) {
+		CHECK(myArrayWithNoDuplicate[successor] == 5);
+	}
+	else
+	{
+		CHECK(false); // force an error record.  
+	}
+	REQUIRE(funcCallCounter > 1); // must be recursive implementation. 
+
+	funcCallCounter = 0;
+	successor = binarySuccessor(myArrayWithNoDuplicate, 0, 8, -3);
+	CHECK(successor != -1);
+	CHECK(successor < 10);
+	if ((successor < 10) && (successor > -1)) {
+		CHECK(myArrayWithNoDuplicate[successor] == -3);
+	}
+	else
+	{
+		CHECK(false); // force an error record.  
+	}
+	REQUIRE(funcCallCounter > 1); // must be recursive implementation. 
+
+
+
+	funcCallCounter = 0;
+	successor = binarySuccessor(myArrayWithNoDuplicate, 0, 8, 10);
+	CHECK(successor == -1);
+	REQUIRE(funcCallCounter > 1);
+
+	funcCallCounter = 0;
+	successor = binarySuccessor(myArrayWithNoDuplicate, 0, 8, -6);
+	CHECK(successor == 0);
+	REQUIRE(funcCallCounter > 1);
 
 }
 
 TEST_CASE("Test Exponetiation", "[expo]") {
 
+	funcCallCounter = 0;
+
+	int result = expo(1, 10);
+	CHECK(result == 1);
+	CHECK(funcCallCounter <= 5);
+	CHECK(funcCallCounter > 2);
+	REQUIRE(funcCallCounter > 1);
+	
+	funcCallCounter = 0;
+	result = expo(-3, 4);
+	CHECK(result == 81);
+	REQUIRE(funcCallCounter > 1);
+	CHECK(funcCallCounter <= 8);
+	CHECK(funcCallCounter > 2);
+
+	funcCallCounter = 0;
+	result = expo(-3, 5);
+	CHECK(result == -243);
+	REQUIRE(funcCallCounter > 1);
+	CHECK(funcCallCounter <= 8);
+	CHECK(funcCallCounter > 2);
+
+	funcCallCounter = 0;
+	result = expo(1, 0);
+	CHECK(result == 1);
+	CHECK(funcCallCounter == 1);
+
+	funcCallCounter = 0;
+	result = expo(0, 1);
+	CHECK(result == 0);
+	CHECK(funcCallCounter == 1);
+
+
 
 }
 
 TEST_CASE("Test String Reverse", "[reverse]") {
+	std::string s = "chdddwwe";
 
+	funcCallCounter = 0;
+	std::string result = reverse(s, s.length());
+	CHECK(result.compare("ewwdddhc") == 0);
+	CHECK(funcCallCounter < 10);
+	REQUIRE(funcCallCounter > 1);
+
+	std::string s1 = "ksjdhe r";
+
+	funcCallCounter = 0;
+	std::string result2 = reverse(s1, s1.length());
+	CHECK(result2.compare("r ehdjsk") == 0);
+	CHECK(funcCallCounter < 10);
+	REQUIRE(funcCallCounter > 1);
 	
-
 
 }
 
 
 TEST_CASE("Test finding the minimum", "[minArray]") {
 
+	funcCallCounter = 0;
+	int myArray[6] = { -2, 5, 3, -5, -7, 0 };
+	int minv = minArray(myArray, 0, 5);
+	CHECK(minv == -7);
+	CHECK(funcCallCounter > 3);
+	REQUIRE(funcCallCounter > 1);
 
+
+	funcCallCounter = 0;
+	int anotherArray[1] = { 3 };
+	minv = minArray(anotherArray, 0, 0);
+	CHECK(minv == 3);
+	CHECK(funcCallCounter == 1);
+
+	funcCallCounter = 0;
+	int anotherArray2[1] = { };
+	minv = minArray(anotherArray2, 0, 0);
+	CHECK(minv == 0);
+	CHECK(funcCallCounter == 1);
 	
 }
 
